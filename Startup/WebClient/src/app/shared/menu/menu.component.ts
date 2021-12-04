6e4e9ef661db8f1fe6f0edc0a3a192418e34ba88
srdjan.ruljic <srdjan.ruljic@gmail.com>
2021-12-04 23:01:20 +0100

    toggleNavbar

--- Startup/WebClient/src/app/shared/menu/menu.component.ts
+++ Startup/WebClient/src/app/shared/menu/menu.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  navbarOpen = false;
 
-  constructor() { }
+  constructor(private _router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
+  goToHome() {
+    this._router.navigate(['/']);
+  }
 }
