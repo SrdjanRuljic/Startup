180c877458ac0124a53175b8c482ff838743eff5
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2021-12-06 12:23:42 +0100

    ngx-bootstrap-navbar,  bootstrap 5

--- Startup/WebClient/src/app/shared/shared.module.ts
+++ Startup/WebClient/src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  imports: [CommonModule, BrowserAnimationsModule, NgxNavbarModule],
+  declarations: [MenuComponent],
+  exports: [MenuComponent],
 })
 export class SharedModule {}
