b4cddb68696aefe9534829b13e333ee6c1d29c2c
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2021-12-06 10:42:47 +0100

    Delete WebClient

--- Startup/WebClient/src/app/core/my-globals.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-
-@Injectable()
-export class MyGlobals {
-  WebApiUrl: string = environment.api_url;
-}
