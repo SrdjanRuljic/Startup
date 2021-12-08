7c7967402f3aebb1cd09196895d6fa41356eb917
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2021-12-08 10:22:10 +0100

    jwt interseptor refactor

--- Startup/WebClient/src/app/core/jwt-interceptor.ts
+++ Startup/WebClient/src/app/core/jwt-interceptor.ts
@@ -10,32 +10,22 @@ import { Observable, take } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../shared/services/auth.service';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: '',
-  }),
-};
-
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  isAuthorized$: Observable<boolean>;
-
-  constructor(private _authService: AuthService) {
-    this.isAuthorized$ = this._authService.getIsAuthorized();
-  }
+  constructor(private _authService: AuthService) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const token = this._authService.getToken();
     const isApiUrl = request.url.startsWith(environment.api_url);
-    const isLoggedIn = this.isAuthorized$.pipe(take(1)) as unknown as boolean;
 
-    if (isLoggedIn && isApiUrl) {
+    if (isApiUrl && token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${this._authService.getToken}`,
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
         },
       });
     }
