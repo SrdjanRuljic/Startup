4429c5279fff0b1f0bbd372116c5c6f7c646c101
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2022-02-15 15:59:15 +0100

    ShouldRequireMinimumFields

---
+++ Startup/Tests/Application.IntegrationTests/Auth/Queries/LoginQueryTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.Auth.Queries.Login;
+using Application.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Auth.Queries
+{
+    using static Testing;
+
+    public class LoginQueryTests : TestBase
+    {
+        [Test]
+        public void ShouldRequireMinimumFields()
+        {
+            var query = new LoginQuery();
+
+            FluentActions.Invoking(() =>
+                SendAsync(query)).Should()
+                                 .ThrowAsync<BadRequestException>();
+        }
+    }
+}
