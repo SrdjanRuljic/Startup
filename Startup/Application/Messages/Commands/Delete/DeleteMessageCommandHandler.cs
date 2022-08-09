38529fdc59b9437ec2baf6474a4598da4a84f6c9
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2022-08-09 08:30:13 +0200

    Removed uneccessery usings

--- Startup/Application/Messages/Commands/Delete/DeleteMessageCommandHandler.cs
+++ Startup/Application/Messages/Commands/Delete/DeleteMessageCommandHandler.cs
@@ -1,12 +1,9 @@
 ﻿using Application.Common.Interfaces;
-using Application.Common.Models;
 using Application.Exceptions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
