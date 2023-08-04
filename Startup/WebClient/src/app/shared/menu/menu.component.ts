8c4a36c9d89b9f74ab2f936f0e45e07bf63e93e4
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2023-08-04 15:10:23 +0200

    Update angular

--- Startup/WebClient/src/app/shared/menu/menu.component.ts
+++ Startup/WebClient/src/app/shared/menu/menu.component.ts
@@ -23,6 +23,7 @@ export class MenuComponent implements OnInit, OnDestroy {
   PERMISSION: typeof PERMISSION = PERMISSION;
   displayName: string = '';
   subscription: Subscription;
+  navbarOpen = false;
 
   constructor(
     private _router: Router,
@@ -82,5 +83,9 @@ export class MenuComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 }
